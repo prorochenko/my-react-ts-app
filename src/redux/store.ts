16896782d71c7aa4afbf896c026c22c9ab36eaca
prorochenko <33101252+prorochenko@users.noreply.github.com>
2023-06-19 20:17:52 +0300

    added basic slices

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,6 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { filtersReducer } from './filterSlice';
-import { contactsReducer } from './filterSlice';
+import { contactsReducer } from './contactSlice';
 
 export const store = configureStore({
   reducer: {
