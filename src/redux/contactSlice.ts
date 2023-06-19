be5d2179be60b02b7594906a37e18b23f4b1ece6
prorochenko <33101252+prorochenko@users.noreply.github.com>
2023-06-19 20:17:11 +0300

    added basic slices

--- src/redux/contactSlice.ts
+++ src/redux/contactSlice.ts
@@ -1,18 +1,42 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, nanoid, PayloadAction } from '@reduxjs/toolkit';
 
-const contactsInitialState = {
-  status: 'all',
-};
+const contactsInitialState = [
+  { id: '0', text: 'John Travolta', tag: 'friend' },
+  { id: '1', text: 'Leonardo Dicaprio', tag: 'family' },
+];
+
+interface Contact {
+  id: string;
+  text: string;
+  tag: string;
+}
+
+type ContactsState = Contact[];
 
 const contactsSlice = createSlice({
   name: 'contacts',
-  initialState: contactsInitialState,
+  initialState: contactsInitialState as ContactsState,
   reducers: {
-    setStatusFilter(state, action) {
-      state.status = action.payload;
+    addContacts: {
+      reducer: (state, action: PayloadAction<Contact>) => {
+        state.push(action.payload);
+      },
+      prepare: (text: string, tag: string) => {
+        return {
+          payload: {
+            id: nanoid(),
+            text,
+            tag,
+          },
+        };
+      },
+    },
+    deleteContacts: (state, action: PayloadAction<string>) => {
+      const index = state.findIndex(contact => contact.id === action.payload);
+      state.splice(index, 1);
     },
   },
 });
 
-export const { setStatusFilter } = contactsSlice.actions;
+export const { addContacts, deleteContacts } = contactsSlice.actions;
 export const contactsReducer = contactsSlice.reducer;
